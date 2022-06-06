44c2034e57c5d8da5fb044e3f33379198f47f433
Philipp Rohde <Philipp.Rohde@tib.eu>
2022-06-06 09:10:50 +0200

    handle both charts the same way

--- fedsdm/static/js/home.js
+++ fedsdm/static/js/home.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
     ******* Load statistics about data sources, RDF-MTs and links ***********
     *************************************************************************
     */
+    $("#federation-summary").hide();
     $("#data-summary").hide();
     $("#summary-row").hide();
     $("#content-row").hide();
@@ -66,144 +67,109 @@ $(document).ready(function() {
                     break;
                 }
             }
-            if (dataSummaryChart == null) {
-                dataSummaryChart = new Chart($("#data-summary-chart"), {
-                    type: 'horizontalBar',
-                    data: {
-                        labels: dsData.labels,
-                        datasets : [
-                            {
-                                id: 1,
-                                label: "# of Triples (log)",
-                                data: dsData.triples,
-                                borderWidth: 1,
-                                backgroundColor: colorNumberTriples
-                            }, {
-                                id: 2,
-                                label: "# of RDF-MTs (log)",
-                                data: dsData.rdfmts,
-                                borderWidth: 1,
-                                backgroundColor: colorNumberMolecules
-                            }, {
-                                id: 3,
-                                label: "# of Properties (log)",
-                                data: dsData.properties,
-                                borderWidth: 1,
-                                backgroundColor: colorNumberProperties
-                            }, {
-                                id: 4,
-                                label: "# of Links (log)",
-                                data: dsData.links,
-                                borderWidth: 1,
-                                backgroundColor: colorNumberLinks
-                            }
-                        ]
-                    },
-                    options: chartOptions
-                });
-                nfeds = nf;
-                nds = nd;
-                for(let [i, v] of nds.entries()) {
-                    let bb = parseInt(v);
-                    nds[i] = Math.log10(bb);
-                }
-                ntpl = nl;
-                for(let [i, v] of ntpl.entries()) {
-                    let bb = parseInt(v);
-                    ntpl[i] = Math.log10(bb);
-                }
-                nftm = nm;
-                for(let [i, v] of nftm.entries()) {
-                    let bb = parseInt(v);
-                    nftm[i] = Math.log10(bb);
-                }
-                nprp = np;
-                for(let [i, v] of nprp.entries()) {
-                    let bb = parseInt(v);
-                    nprp[i] = Math.log10(bb);
-                }
-                nlnk = nk;
-                for(let [i, v] of nlnk.entries()) {
-                    let bb = parseInt(v);
-                    nlnk[i] = Math.log10(bb);
-                }
-                federationSummaryChart = new Chart($("#federation-summary-chart"), {
-                    type: 'horizontalBar',
-                    data: {
-                        labels: nfeds,
-                        datasets : [
-                            {
-                                id: 1,
-                                label: "# of Data Sources (log)",
-                                data: nds,
-                                borderWidth: 1,
-                                backgroundColor: colorNumberSources
-                            }, {
-                                id: 2,
-                                label: "# of Triples (log)",
-                                data: ntpl,
-                                borderWidth: 1,
-                                backgroundColor: colorNumberTriples
-                            }, {
-                                id: 3,
-                                label: "# of RDF-MTs (log)",
-                                data: nftm,
-                                borderWidth: 1,
-                                backgroundColor: colorNumberMolecules
-                            }, {
-                                id: 4,
-                                label: "# of Properties (log)",
-                                data: nprp,
-                                borderWidth: 1,
-                                backgroundColor: colorNumberProperties
-                            }, {
-                                id: 5,
-                                label: "# of Links (log)",
-                                data: nlnk,
-                                borderWidth: 1,
-                                backgroundColor: colorNumberLinks
-                            }
-                        ]
-                    },
-                    options: chartOptions
-                });
-            } else {
-                dataSummaryChart.data.labels = [];
-                dataSummaryChart.data.datasets = [];
-                dataSummaryChart.update();
+            dataSummaryChart = new Chart($("#data-summary-chart"), {
+                type: 'horizontalBar',
+                data: {
+                    labels: dsData.labels,
+                    datasets : [
+                        {
+                            id: 1,
+                            label: "# of Triples (log)",
+                            data: dsData.triples,
+                            borderWidth: 1,
+                            backgroundColor: colorNumberTriples
+                        }, {
+                            id: 2,
+                            label: "# of RDF-MTs (log)",
+                            data: dsData.rdfmts,
+                            borderWidth: 1,
+                            backgroundColor: colorNumberMolecules
+                        }, {
+                            id: 3,
+                            label: "# of Properties (log)",
+                            data: dsData.properties,
+                            borderWidth: 1,
+                            backgroundColor: colorNumberProperties
+                        }, {
+                            id: 4,
+                            label: "# of Links (log)",
+                            data: dsData.links,
+                            borderWidth: 1,
+                            backgroundColor: colorNumberLinks
+                        }
+                    ]
+                },
+                options: chartOptions
+            });
 
-                dataSummaryChart.data.labels = dsData.labels;
-                dataSummaryChart.data.datasets = [
-                    {
-                        id: 1,
-                        label: "# of Triples (log)",
-                        data: dsData.triples,
-                        borderWidth: 1,
-                        backgroundColor: colorNumberTriples
-                    }, {
-                        id: 2,
-                        label: "# of RDF-MTs (log)",
-                        data: dsData.rdfmts,
-                        borderWidth: 1,
-                        backgroundColor: colorNumberMolecules
-                    }, {
-                        id: 3,
-                        label: "# of Properties (log)",
-                        data: dsData.properties,
-                        borderWidth: 1,
-                        backgroundColor: colorNumberProperties
-                    }, {
-                        id: 4,
-                        label: "# of Links (log)",
-                        data: dsData.links,
-                        borderWidth: 1,
-                        backgroundColor: colorNumberLinks
-                    }
-                ]
-                dataSummaryChart.update();
-                federationSummaryChart.update();
+            nfeds = nf;
+            nds = nd;
+            for(let [i, v] of nds.entries()) {
+                let bb = parseInt(v);
+                nds[i] = Math.log10(bb);
             }
+            ntpl = nl;
+            for(let [i, v] of ntpl.entries()) {
+                let bb = parseInt(v);
+                ntpl[i] = Math.log10(bb);
+            }
+            nftm = nm;
+            for(let [i, v] of nftm.entries()) {
+                let bb = parseInt(v);
+                nftm[i] = Math.log10(bb);
+            }
+            nprp = np;
+            for(let [i, v] of nprp.entries()) {
+                let bb = parseInt(v);
+                nprp[i] = Math.log10(bb);
+            }
+            nlnk = nk;
+            for(let [i, v] of nlnk.entries()) {
+                let bb = parseInt(v);
+                nlnk[i] = Math.log10(bb);
+            }
+            federationSummaryChart = new Chart($("#federation-summary-chart"), {
+                type: 'horizontalBar',
+                data: {
+                    labels: nfeds,
+                    datasets : [
+                        {
+                            id: 1,
+                            label: "# of Data Sources (log)",
+                            data: nds,
+                            borderWidth: 1,
+                            backgroundColor: colorNumberSources
+                        }, {
+                            id: 2,
+                            label: "# of Triples (log)",
+                            data: ntpl,
+                            borderWidth: 1,
+                            backgroundColor: colorNumberTriples
+                        }, {
+                            id: 3,
+                            label: "# of RDF-MTs (log)",
+                            data: nftm,
+                            borderWidth: 1,
+                            backgroundColor: colorNumberMolecules
+                        }, {
+                            id: 4,
+                            label: "# of Properties (log)",
+                            data: nprp,
+                            borderWidth: 1,
+                            backgroundColor: colorNumberProperties
+                        }, {
+                            id: 5,
+                            label: "# of Links (log)",
+                            data: nlnk,
+                            borderWidth: 1,
+                            backgroundColor: colorNumberLinks
+                        }
+                    ]
+                },
+                options: chartOptions
+            });
         }
+        $("#federation-summary").show();
         $("#data-summary").show();
     }
 
