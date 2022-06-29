2ea36f7989ecbdb818376f5f02e290886aed1fec
Philipp Rohde <Philipp.Rohde@tib.eu>
2022-06-29 15:24:57 +0200

    prefer single quotes

--- fedsdm/static/js/home.js
+++ fedsdm/static/js/home.js
@@ -4,19 +4,19 @@ $(function() {
     ******* Load statistics about data sources, RDF-MTs and links ***********
     *************************************************************************
     */
-    $("#federation-summary").hide();
-    $("#data-summary").hide();
-    $("#summary-row").hide();
-    $("#content-row").hide();
+    $('#federation-summary').hide();
+    $('#data-summary').hide();
+    $('#summary-row').hide();
+    $('#content-row').hide();
     let dataSummaryChart = null,
         federationSummaryChart = null;
 
     window.setFederation = function(dataSources, federations) {
         if (dataSources != null && federations != null) {
-            $("#summary-row").show();
+            $('#summary-row').show();
             $('#content-row').show();
-            $("#federation-summary").show();
-            $("#data-summary").show();
+            $('#federation-summary').show();
+            $('#data-summary').show();
 
             let dsData = {labels: [], rdfmts: [], links: [], properties: [], triples: []};
             for (let i in dataSources) {  // in JavaScript this will return the index and not the element
@@ -30,32 +30,32 @@ $(function() {
                     break;
                 }
             }
-            dataSummaryChart = new Chart($("#data-summary-chart"), {
+            dataSummaryChart = new Chart($('#data-summary-chart'), {
                 type: 'bar',
                 data: {
                     labels: dsData.labels,
                     datasets : [
                         {
                             id: 1,
-                            label: "# of Triples (log)",
+                            label: '# of Triples (log)',
                             data: dsData.triples,
                             borderWidth: 1,
                             backgroundColor: colorNumberTriples
                         }, {
                             id: 2,
-                            label: "# of RDF-MTs (log)",
+                            label: '# of RDF-MTs (log)',
                             data: dsData.rdfmts,
                             borderWidth: 1,
                             backgroundColor: colorNumberMolecules
                         }, {
                             id: 3,
-                            label: "# of Properties (log)",
+                            label: '# of Properties (log)',
                             data: dsData.properties,
                             borderWidth: 1,
                             backgroundColor: colorNumberProperties
                         }, {
                             id: 4,
-                            label: "# of Links (log)",
+                            label: '# of Links (log)',
                             data: dsData.links,
                             borderWidth: 1,
                             backgroundColor: colorNumberLinks
@@ -75,38 +75,38 @@ $(function() {
                 fedData.links.push(log10(fed.properties));
                 fedData.triples.push(log10(fed.triples))
             }
-            federationSummaryChart = new Chart($("#federation-summary-chart"), {
+            federationSummaryChart = new Chart($('#federation-summary-chart'), {
                 type: 'bar',
                 data: {
                     labels: fedData.labels,
                     datasets : [
                         {
                             id: 1,
-                            label: "# of Data Sources (log)",
+                            label: '# of Data Sources (log)',
                             data: fedData.sources,
                             borderWidth: 1,
                             backgroundColor: colorNumberSources
                         }, {
                             id: 2,
-                            label: "# of Triples (log)",
+                            label: '# of Triples (log)',
                             data: fedData.triples,
                             borderWidth: 1,
                             backgroundColor: colorNumberTriples
                         }, {
                             id: 3,
-                            label: "# of RDF-MTs (log)",
+                            label: '# of RDF-MTs (log)',
                             data: fedData.rdfmts,
                             borderWidth: 1,
                             backgroundColor: colorNumberMolecules
                         }, {
                             id: 4,
-                            label: "# of Properties (log)",
+                            label: '# of Properties (log)',
                             data: fedData.properties,
                             borderWidth: 1,
                             backgroundColor: colorNumberProperties
                         }, {
                             id: 5,
-                            label: "# of Links (log)",
+                            label: '# of Links (log)',
                             data: fedData.links,
                             borderWidth: 1,
                             backgroundColor: colorNumberLinks
@@ -119,9 +119,9 @@ $(function() {
     }
 
     window.setStats = function(stats) {
-        $("#total-data-sources").html(stats.sources);
-        $("#total-rdfmts").html(stats.rdfmts);
-        $("#total-links-rdfmts").html(stats.links);
-        $("#total-federations").html(stats.federations);
+        $('#total-data-sources').html(stats.sources);
+        $('#total-rdfmts').html(stats.rdfmts);
+        $('#total-links-rdfmts').html(stats.links);
+        $('#total-federations').html(stats.federations);
     }
 });
