557006430904eee6261ec9b5acaeadbed14ce009
Philipp Rohde <Philipp.Rohde@tib.eu>
2022-06-30 10:55:46 +0200

    consistent use of quotation marks

--- fedsdm/static/js/menu-setup.js
+++ fedsdm/static/js/menu-setup.js
@@ -8,7 +8,7 @@
 //collapses the sidebar on window resize.
 // Sets the min-height of #page-wrapper to window size
 $(function() {
-    $(window).on("load resize", function() {
+    $(window).on('load resize', function() {
         var topOffset = 50;
         var width = (this.window.innerWidth > 0) ? this.window.innerWidth : this.screen.width;
         if (width < 768) {
@@ -22,7 +22,7 @@ $(function() {
         height = height - topOffset;
         if (height < 1) height = 1;
         if (height > topOffset) {
-            $("#page-wrapper").css("min-height", (height) + "px");
+            $('#page-wrapper').css('min-height', (height) + 'px');
         }
     });
 
