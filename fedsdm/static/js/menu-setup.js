1637be69b26b6301b257b7508045900256ef683c
Philipp Rohde <Philipp.Rohde@tib.eu>
2022-06-30 15:13:46 +0200

    improve code quality

--- fedsdm/static/js/menu-setup.js
+++ fedsdm/static/js/menu-setup.js
@@ -4,13 +4,13 @@
  * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap/blob/gh-pages/LICENSE)
  */
 
-//Loads the correct sidebar on window load,
-//collapses the sidebar on window resize.
+// Loads the correct sidebar on window load,
+// collapses the sidebar on window resize.
 // Sets the min-height of #page-wrapper to window size
 $(function() {
     $(window).on('load resize', function() {
-        var topOffset = 50;
-        var width = (this.window.innerWidth > 0) ? this.window.innerWidth : this.screen.width;
+        let topOffset = 50;
+        let width = (this.window.innerWidth > 0) ? this.window.innerWidth : this.screen.width;
         if (width < 768) {
             $('div.navbar-collapse').addClass('collapse');
             topOffset = 100; // 2-row-menu
@@ -18,7 +18,7 @@ $(function() {
             $('div.navbar-collapse').removeClass('collapse');
         }
 
-        var height = ((this.window.innerHeight > 0) ? this.window.innerHeight : this.screen.height) - 1;
+        let height = ((this.window.innerHeight > 0) ? this.window.innerHeight : this.screen.height) - 1;
         height = height - topOffset;
         if (height < 1) height = 1;
         if (height > topOffset) {
@@ -26,12 +26,9 @@ $(function() {
         }
     });
 
-    var url = window.location;
-    // var element = $('ul.nav a').filter(function() {
-    //     return this.href == url;
-    // }).addClass('active').parent().parent().addClass('in').parent();
-    var element = $('ul.nav a').filter(function() {
-        return this.href == url;
+    let url = window.location;
+    let element = $('ul.nav a').filter(function() {
+        return this.href === url;
     }).addClass('active').parent();
 
     while (true) {
