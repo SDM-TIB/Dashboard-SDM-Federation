ebe179bdd185d633f109aa91196ca375e18e47df
Philipp Rohde <Philipp.Rohde@tib.eu>
2022-07-13 16:41:02 +0200

    prefer single quotes

--- fedsdm/ui/utils.py
+++ fedsdm/ui/utils.py
@@ -7,7 +7,7 @@ def _process_numeric_result(mdb, query):
     if card > 0:
         card = res[0]['count']
         if '^^' in card:
-            card = int(card[:card.find("^^")])
+            card = int(card[:card.find('^^')])
         return int(card)
     else:
         return 0
@@ -16,31 +16,31 @@ def _process_numeric_result(mdb, query):
 def get_federations():
     mdb = get_mdb()
 
-    query = "SELECT DISTINCT ?uri ?name WHERE {" \
-            " GRAPH <" + g.default_graph + "> { ?uri a <" + mdb.mtonto + "Federation>. " \
-                                           " ?uri <" + mdb.mtonto + "name> ?name } }"
+    query = 'SELECT DISTINCT ?uri ?name WHERE {' \
+            ' GRAPH <' + g.default_graph + '> { ?uri a <' + mdb.mtonto + 'Federation>. ' \
+                                           ' ?uri <' + mdb.mtonto + 'name> ?name } }'
     res, card = mdb.query(query)
     if card > 0:
         return res
     else:
-        print("no federations available ...")
+        print('no federations available...')
         return []
 
 
 def get_datasources(graph=None):
     mdb = get_mdb()
     if graph is not None:
-        query = "SELECT DISTINCT ?uri ?source ?triples WHERE { GRAPH <" + graph + "> { " \
-                    "?uri a <" + mdb.mtonto + "DataSource> . " \
-                    "?uri  <" + mdb.mtonto + "name> ?source . " \
-                    "OPTIONAL {?uri <" + mdb.mtonto + "triples> ?triples .} " \
-                    "}}"
+        query = 'SELECT DISTINCT ?uri ?source ?triples WHERE { GRAPH <' + graph + '> { ' \
+                    '?uri a <' + mdb.mtonto + 'DataSource> . ' \
+                    '?uri  <' + mdb.mtonto + 'name> ?source . ' \
+                    'OPTIONAL {?uri <' + mdb.mtonto + 'triples> ?triples .} ' \
+                    '}}'
     else:
-        query = "SELECT DISTINCT ?uri ?source ?triples WHERE {" \
-                "    ?uri a <" + mdb.mtonto + "DataSource>." \
-                "    ?uri  <" + mdb.mtonto + "name> ?source . " \
-                "    ?uri <" + mdb.mtonto + "triples> ?triples ." \
-                "   }"
+        query = 'SELECT DISTINCT ?uri ?source ?triples WHERE {' \
+                '    ?uri a <' + mdb.mtonto + 'DataSource>.' \
+                '    ?uri  <' + mdb.mtonto + 'name> ?source . ' \
+                '    ?uri <' + mdb.mtonto + 'triples> ?triples .' \
+                '   }'
     res, card = mdb.query(query)
     if card > 0:
         return {d['uri']: d for d in res}
@@ -51,14 +51,14 @@ def get_datasources(graph=None):
 def get_num_rdfmts(graph, datasource=None):
     mdb = get_mdb()
     if datasource is not None:
-        query = " SELECT (COUNT (DISTINCT ?mt) AS ?count) WHERE { GRAPH <" + graph + "> { ?mt a <" + mdb.mtonto + "RDFMT> ." \
-                "           ?mt  <" + mdb.mtonto + "source>  ?mtsource. "\
-                "           ?mtsource <" + mdb.mtonto + "datasource> <" + datasource + "> . } }"
+        query = ' SELECT (COUNT (DISTINCT ?mt) AS ?count) WHERE { GRAPH <' + graph + '> { ?mt a <' + mdb.mtonto + 'RDFMT> .' \
+                '           ?mt  <' + mdb.mtonto + 'source>  ?mtsource. '\
+                '           ?mtsource <' + mdb.mtonto + 'datasource> <' + datasource + '> . } }'
 
     else:
-        query = " SELECT (COUNT (DISTINCT ?mt) AS ?count) WHERE { GRAPH <" + graph + "> { ?mt a <" + mdb.mtonto + "RDFMT> ." \
-               "           ?mt  <" + mdb.mtonto + "source>  ?mtsource. " \
-               "           ?mtsource <" + mdb.mtonto + "datasource> ?ds . } }"
+        query = ' SELECT (COUNT (DISTINCT ?mt) AS ?count) WHERE { GRAPH <' + graph + '> { ?mt a <' + mdb.mtonto + 'RDFMT> .' \
+                '           ?mt  <' + mdb.mtonto + 'source>  ?mtsource. ' \
+                '           ?mtsource <' + mdb.mtonto + 'datasource> ?ds . } }'
 
     return _process_numeric_result(mdb, query)
 
@@ -66,17 +66,17 @@ def get_num_rdfmts(graph, datasource=None):
 def get_mtconns(graph, datasource=None):
     mdb = get_mdb()
     if datasource is not None:
-        query = "SELECT  (COUNT(DISTINCT ?d) as ?count) WHERE { GRAPH <" + graph + "> {" \
-            " ?d a <" + mdb.mtonto + "PropRange> . " \
-            " ?d <" + mdb.mtonto + "name> ?mt ." \
-            " ?d <" + mdb.mtonto + "datasource> <" + datasource +"> ." \
-            " }}"
+        query = 'SELECT  (COUNT(DISTINCT ?d) as ?count) WHERE { GRAPH <' + graph + '> {' \
+            ' ?d a <' + mdb.mtonto + 'PropRange> . ' \
+            ' ?d <' + mdb.mtonto + 'name> ?mt .' \
+            ' ?d <' + mdb.mtonto + 'datasource> <' + datasource + '> .' \
+            ' }}'
     else:
-        query = "SELECT  (COUNT(DISTINCT ?d) as ?count) WHERE { GRAPH <" + graph + "> {" \
-                  "?d a <" + mdb.mtonto + "PropRange> . " \
-                  "?d <" + mdb.mtonto + "name> ?mt ." \
-                  "?d <" + mdb.mtonto + "datasource> ?ds ." \
-                "}}"
+        query = 'SELECT  (COUNT(DISTINCT ?d) as ?count) WHERE { GRAPH <' + graph + '> {' \
+                  '?d a <' + mdb.mtonto + 'PropRange> . ' \
+                  '?d <' + mdb.mtonto + 'name> ?mt .' \
+                  '?d <' + mdb.mtonto + 'datasource> ?ds .' \
+                '}}'
 
     return _process_numeric_result(mdb, query)
 
@@ -84,53 +84,53 @@ def get_mtconns(graph, datasource=None):
 def get_num_properties(graph, datasource=None):
     mdb = get_mdb()
     if datasource is not None:
-        query = " SELECT (COUNT (DISTINCT ?mtp) AS ?count) WHERE { GRAPH <" + graph + "> { " \
-                "           ?mt a <" + mdb.mtonto + "RDFMT> ." \
-                "           ?mt  <" + mdb.mtonto + "source>  ?mtsource. " \
-                "           ?mt <" + mdb.mtonto + "hasProperty> ?mtp . " \
-                "           ?mtsource <" + mdb.mtonto + "datasource> <" + datasource + "> . } }"
+        query = ' SELECT (COUNT (DISTINCT ?mtp) AS ?count) WHERE { GRAPH <' + graph + '> { ' \
+                '           ?mt a <' + mdb.mtonto + 'RDFMT> .' \
+                '           ?mt  <' + mdb.mtonto + 'source>  ?mtsource. ' \
+                '           ?mt <' + mdb.mtonto + 'hasProperty> ?mtp . ' \
+                '           ?mtsource <' + mdb.mtonto + 'datasource> <' + datasource + '> . } }'
 
     else:
-        query = " SELECT (COUNT (DISTINCT ?mtp) AS ?count) WHERE { GRAPH <" + graph + "> { " \
-                  "           ?mt a <" + mdb.mtonto + "RDFMT> ." \
-                  "           ?mt  <" + mdb.mtonto + "source>  ?mtsource. " \
-                  "           ?mt <" + mdb.mtonto + "hasProperty> ?mtp . " \
-                  "           ?mtsource <" + mdb.mtonto + "datasource> ?ds . } }"
+        query = ' SELECT (COUNT (DISTINCT ?mtp) AS ?count) WHERE { GRAPH <' + graph + '> { ' \
+                '           ?mt a <' + mdb.mtonto + 'RDFMT> .' \
+                '           ?mt  <' + mdb.mtonto + 'source>  ?mtsource. ' \
+                '           ?mt <' + mdb.mtonto + 'hasProperty> ?mtp . ' \
+                '           ?mtsource <' + mdb.mtonto + 'datasource> ?ds . } }'
 
     return _process_numeric_result(mdb, query)
 
 
 def get_federation_stats():
     mdb = get_mdb()
-    query = "SELECT DISTINCT ?fed ?name (COUNT(DISTINCT ?ds) AS ?sources) (SUM(COALESCE(?count_mts, 0)) AS ?rdfmts) " \
-            "(SUM(COALESCE(?count_links, 0)) AS ?links) (SUM(COALESCE(?count_prop, 0)) AS ?properties) " \
-            "(SUM(COALESCE(?ds_triples, 0)) AS ?triples) WHERE {" \
-            "  ?fed a <" + mdb.mtonto + "Federation> ." \
-            "  ?fed <" + mdb.mtonto + "name> ?name ." \
-            "  OPTIONAL { SELECT DISTINCT ?fed ?ds (COUNT (DISTINCT ?mt) AS ?count_mts) " \
-            "    (COUNT(DISTINCT ?d) as ?count_links) (COUNT (DISTINCT ?mtp) AS ?count_prop) WHERE {" \
-            "      GRAPH ?fed {" \
-            "        OPTIONAL { " \
-            "          ?ds a <" + mdb.mtonto + "DataSource> . " \
-            "          OPTIONAL {" \
-            "            ?d a <" + mdb.mtonto + "PropRange> ." \
-            "            ?d <" + mdb.mtonto + "datasource> ?ds ." \
-            "          }" \
-            "        }" \
-            "        OPTIONAL {" \
-            "          ?mt a <" + mdb.mtonto + "RDFMT> ." \
-            "          ?mt <" + mdb.mtonto + "source> ?mtsource ." \
-            "          ?mt <" + mdb.mtonto + "hasProperty> ?mtp ." \
-            "          ?mtsource <" + mdb.mtonto + "datasource> ?ds ." \
-            "        }" \
-            "      }" \
-            "  } GROUP BY ?fed ?ds }" \
-            "  OPTIONAL { SELECT DISTINCT ?fed ?ds ?ds_triples WHERE {" \
-            "    GRAPH ?fed {" \
-            "      ?ds <" + mdb.mtonto + "triples> ?ds_triples ." \
-            "    }" \
-            "  }}" \
-            "} GROUP BY ?fed ?name"
+    query = 'SELECT DISTINCT ?fed ?name (COUNT(DISTINCT ?ds) AS ?sources) (SUM(COALESCE(?count_mts, 0)) AS ?rdfmts) ' \
+            '(SUM(COALESCE(?count_links, 0)) AS ?links) (SUM(COALESCE(?count_prop, 0)) AS ?properties) ' \
+            '(SUM(COALESCE(?ds_triples, 0)) AS ?triples) WHERE {' \
+            '  ?fed a <' + mdb.mtonto + 'Federation> .' \
+            '  ?fed <' + mdb.mtonto + 'name> ?name .' \
+            '  OPTIONAL { SELECT DISTINCT ?fed ?ds (COUNT (DISTINCT ?mt) AS ?count_mts) ' \
+            '    (COUNT(DISTINCT ?d) as ?count_links) (COUNT (DISTINCT ?mtp) AS ?count_prop) WHERE {' \
+            '      GRAPH ?fed {' \
+            '        OPTIONAL { ' \
+            '          ?ds a <' + mdb.mtonto + 'DataSource> . ' \
+            '          OPTIONAL {' \
+            '            ?d a <' + mdb.mtonto + 'PropRange> .' \
+            '            ?d <' + mdb.mtonto + 'datasource> ?ds .' \
+            '          }' \
+            '        }' \
+            '        OPTIONAL {' \
+            '          ?mt a <' + mdb.mtonto + 'RDFMT> .' \
+            '          ?mt <' + mdb.mtonto + 'source> ?mtsource .' \
+            '          ?mt <' + mdb.mtonto + 'hasProperty> ?mtp .' \
+            '          ?mtsource <' + mdb.mtonto + 'datasource> ?ds .' \
+            '        }' \
+            '      }' \
+            '  } GROUP BY ?fed ?ds }' \
+            '  OPTIONAL { SELECT DISTINCT ?fed ?ds ?ds_triples WHERE {' \
+            '    GRAPH ?fed {' \
+            '      ?ds <' + mdb.mtonto + 'triples> ?ds_triples .' \
+            '    }' \
+            '  }}' \
+            '} GROUP BY ?fed ?name'
 
     from fedsdm import get_logger
     logger = get_logger(__name__)
