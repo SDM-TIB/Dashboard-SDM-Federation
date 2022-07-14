19a205ddac989dd0707f872ae1e5b84889343d97
Philipp Rohde <Philipp.Rohde@tib.eu>
2022-07-14 11:09:18 +0200

    fix indentation

--- fedsdm/auth.py
+++ fedsdm/auth.py
@@ -51,13 +51,11 @@ def register():
         error = None
 
         if not username:
-            error = 'Username is required. '
+            error = 'Username is required.'
         elif not password:
-            error = 'Password is required. '
-        elif db.execute('SELECT id FROM user WHERE username = ?',
-                        (username, )
-                        ).fetchone() is not None:
-            error = 'User {} is already registered . '.format(username)
+            error = 'Password is required.'
+        elif db.execute('SELECT id FROM user WHERE username = ?', (username, )).fetchone() is not None:
+            error = 'User {} is already registered.'.format(username)
 
         if error is None:
             db.execute(
@@ -92,9 +90,7 @@ def login():
         password = request.form['password']
         db = get_db()
         error = None
-        user = db.execute(
-            "SELECT * FROm user WHERE username = ?", (username, )
-        ).fetchone()
+        user = db.execute("SELECT * FROm user WHERE username = ?", (username, )).fetchone()
 
         if user is None:
             error = "Incorrect username. "
@@ -127,13 +123,10 @@ def load_logged_in_user():
     if user_id is None:
         g.user = None
     else:
-        g.user = get_db().execute(
-            "SELECT * FROM user WHERE id = ?", (user_id, )
-        ).fetchone()
+        g.user = get_db().execute("SELECT * FROM user WHERE id = ?", (user_id, )).fetchone()
 
 
 # Logout
-
 @bp.route('/logout')
 def logout():
     """
