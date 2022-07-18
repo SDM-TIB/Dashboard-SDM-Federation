9f64bb0919a0cfe958688d5c1a080800da43b843
Philipp Rohde <Philipp.Rohde@tib.eu>
2022-07-18 07:53:49 +0200

    prefer single quotes

--- fedsdm/db.py
+++ fedsdm/db.py
@@ -21,65 +21,65 @@ class MetadataDB:
             self.update_endpoint = query_endpoint
 
         if 'https' in query_endpoint:
-            server = query_endpoint.split("https://")[1]
+            server = query_endpoint.split('https://')[1]
         else:
-            server = query_endpoint.split("http://")[1]
+            server = query_endpoint.split('http://')[1]
 
-        (server, path) = server.split("/", 1)
+        (server, path) = server.split('/', 1)
         self.query_server = server
         self.query_path = path
 
         if 'https' in self.update_endpoint:
-            server = self.update_endpoint.split("https://")[1]
+            server = self.update_endpoint.split('https://')[1]
         else:
-            server = self.update_endpoint.split("http://")[1]
+            server = self.update_endpoint.split('http://')[1]
 
-        (server, path) = server.split("/", 1)
+        (server, path) = server.split('/', 1)
         self.update_server = server
         self.update_path = path
-        self.xsd = "http://www.w3.org/2001/XMLSchema#"
-        self.owl = ""
-        self.rdf = ""
-        self.rdfs = "http://www.w3.org/2000/01/rdf-schema#"
-        self.mtonto = "http://tib.eu/dsdl/ontario/ontology/"
-        self.mtresource = "http://tib.eu/dsdl/ontario/resource/"
+        self.xsd = 'http://www.w3.org/2001/XMLSchema#'
+        self.owl = ''
+        self.rdf = ''
+        self.rdfs = 'http://www.w3.org/2000/01/rdf-schema#'
+        self.mtonto = 'http://tib.eu/dsdl/ontario/ontology/'
+        self.mtresource = 'http://tib.eu/dsdl/ontario/resource/'
         self.prefixes = 'PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\n' \
                         'PREFIX mt: <http://tib.eu/dsdl/ontario/ontology/>\n' \
                         'PREFIX mtres: <http://tib.eu/dsdl/ontario/resource/>\n'
 
-    def query(self, query, outputqueue=Queue(), format="application/sparql-results+json"):
+    def query(self, query, outputqueue=Queue(), format='application/sparql-results+json'):
         # Build the query and header.
         query = self.prefixes + query
         params = urlparse.urlencode({'query': query, 'format': format, 'timeout': 10000000})
-        headers = {"Accept": "*/*", "Referer": self.query_endpoint, "Host": self.query_server}
+        headers = {'Accept': '*/*', 'Referer': self.query_endpoint, 'Host': self.query_server}
 
         try:
             resp = requests.get(self.query_endpoint, params=params, headers=headers)
             if resp.status_code == HTTPStatus.OK:
                 res = resp.text
                 reslist = []
-                if format != "application/sparql-results+json":
+                if format != 'application/sparql-results+json':
                     return res
 
                 try:
-                    res = res.replace("false", "False")
-                    res = res.replace("true", "True")
+                    res = res.replace('false', 'False')
+                    res = res.replace('true', 'True')
                     res = eval(res)
                 except Exception as ex:
-                    print("EX processing res", ex)
+                    print('EX processing res', ex)
 
                 if type(res) is dict:
-                    if "results" in res:
+                    if 'results' in res:
                         for x in res['results']['bindings']:
                             for key, props in x.items():
                                 # Handle typed-literals and language tags
                                 suffix = ''
                                 if props['type'] == 'typed-literal':
                                     if isinstance(props['datatype'], bytes):
-                                        suffix = ''  # "^^<" + props['datatype'].decode('utf-8') + ">"
+                                        suffix = ''  # '^^<' + props['datatype'].decode('utf-8') + '>'
                                     else:
-                                        suffix = ''  # "^^<" + props['datatype'] + ">"
-                                elif "xml:lang" in props:
+                                        suffix = ''  # '^^<' + props['datatype'] + '>'
+                                elif 'xml:lang' in props:
                                     suffix = ''  # '@' + props['xml:lang']
                                 try:
                                     if isinstance(props['value'], bytes):
@@ -101,37 +101,37 @@ class MetadataDB:
                         return res['boolean'], 1
 
             else:
-                print("Endpoint->", self.query_endpoint, resp.reason, resp.status_code, query)
+                print('Endpoint->', self.query_endpoint, resp.reason, resp.status_code, query)
 
         except Exception as e:
-            print("Exception during query execution to", self.query_endpoint, ":", e)
+            print('Exception during query execution to', self.query_endpoint, ':', e)
 
         return None, -2
 
     def update(self, insertquery):
         # Build the header.
         insertquery = self.prefixes + insertquery
-        headers = {"Accept": "*/*",
-                   "Referer": self.update_endpoint,
-                   "Host": self.update_server,
-                   "Content-type": "application/sparql-update"}
+        headers = {'Accept': '*/*',
+                   'Referer': self.update_endpoint,
+                   'Host': self.update_server,
+                   'Content-type': 'application/sparql-update'}
 
         try:
             resp = requests.post(self.update_endpoint, data=insertquery, headers=headers)
             if resp.status_code == HTTPStatus.OK or resp.status_code == HTTPStatus.ACCEPTED or resp.status_code == HTTPStatus.NO_CONTENT:
                 return True
             else:
-                print("Update Endpoint->", self.update_endpoint, resp.reason, resp.status_code, insertquery)
-                logger.error("______/_________/________/________/______________")
+                print('Update Endpoint->', self.update_endpoint, resp.reason, resp.status_code, insertquery)
+                logger.error('______/_________/________/________/______________')
                 logger.error(self.update_endpoint + " - " + str(resp.reason) + " - " + str(resp.status_code))
-                logger.error("ERROR ON: " + insertquery)
-                logger.error("________________________________________________")
+                logger.error('ERROR ON: ' + insertquery)
+                logger.error('________________________________________________')
         except Exception as e:
-            print("Exception during update query execution to", self.update_endpoint, ":", e, insertquery)
-            logger.error("______/_________/________/________/______________")
-            logger.error("Exception on update: " + self.update_endpoint + " " + str(e))
-            logger.error("EXCEPTION ON: " + insertquery)
-            logger.error("________________________________________________")
+            print('Exception during update query execution to', self.update_endpoint, ':', e, insertquery)
+            logger.error('______/_________/________/________/______________')
+            logger.error('Exception on update: ' + self.update_endpoint + ' ' + str(e))
+            logger.error('EXCEPTION ON: ' + insertquery)
+            logger.error('________________________________________________')
 
         return False
 
@@ -139,7 +139,7 @@ class MetadataDB:
 def get_db():
     if 'db' not in g:
         g.db = sqlite3.connect(
-            current_app.config["DATABASE"],
+            current_app.config['DATABASE'],
             detect_types=sqlite3.PARSE_DECLTYPES
         )
         g.db.row_factory = sqlite3.Row
@@ -154,7 +154,7 @@ def get_mdb():
             os.environ['METADATA_ENDPOINT'] != "":
         meta_endpoint = os.environ['METADATA_ENDPOINT']
     else:
-        meta_endpoint = "http://localhost:9000/sparql"
+        meta_endpoint = 'http://localhost:9000/sparql'
 
     if 'mdb' not in g:
         g.mdb = MetadataDB(meta_endpoint)
@@ -162,7 +162,7 @@ def get_mdb():
         if 'DEFAULT_GRAPH' in os.environ:
             g.default_graph = os.environ['DEFAULT_GRAPH']
         else:
-            g.default_graph = "http://ontario.tib.eu"
+            g.default_graph = 'http://ontario.tib.eu'
     return g.mdb
 
 
@@ -175,7 +175,7 @@ def close_db(e=None):
 
 def init_db():
     db_nonexistent = False
-    if not os.path.isfile(current_app.config["DATABASE"]):
+    if not os.path.isfile(current_app.config['DATABASE']):
         db_nonexistent = True
 
     db = get_db()
