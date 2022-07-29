bc7559addaa50bb750d4b3b1ba0e8b068ae44abb
Philipp Rohde <Philipp.Rohde@tib.eu>
2022-07-29 11:24:48 +0200

    fix typo

--- FedSDM/auth.py
+++ FedSDM/auth.py
@@ -89,7 +89,7 @@ def login():
         password = request.form['password']
         db = get_db()
         error = None
-        user = db.execute('SELECT * FROm user WHERE username = ?', (username, )).fetchone()
+        user = db.execute('SELECT * FROM user WHERE username = ?', (username, )).fetchone()
 
         if user is None:
             error = 'Incorrect username. '
