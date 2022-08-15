2e8928142447e83cbc36a3d1866d2fd056b409ad
Philipp Rohde <Philipp.Rohde@tib.eu>
2022-08-15 08:00:29 +0200

    move comment into code line

--- FedSDM/db.py
+++ FedSDM/db.py
@@ -163,8 +163,7 @@ def init_db():
 
     db = get_db()
 
-    if db_nonexistent:
-        # database did not exist; initialize database structure
+    if db_nonexistent:  # database did not exist; initialize database structure
         with current_app.open_resource('schema.sql') as f:
             db.executescript(f.read().decode('utf8'))
 
