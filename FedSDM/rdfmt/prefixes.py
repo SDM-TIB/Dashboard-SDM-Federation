6e67432c967d224db6700769824b1fa55bb2ab3b
Philipp Rohde <Philipp.Rohde@tib.eu>
2022-08-24 07:57:11 +0200

    add documentation comments

--- FedSDM/rdfmt/prefixes.py
+++ FedSDM/rdfmt/prefixes.py
@@ -1,4 +1,8 @@
 XSD = 'http://www.w3.org/2001/XMLSchema#'
+"""str: Prefix for XSD; commonly used to specify datatypes."""
 RDFS = 'http://www.w3.org/2000/01/rdf-schema#'
+"""str: Prefix for RDF Schema; e.g., subclasses, and domain and range of predicates"""
 MT_ONTO = 'http://tib.eu/dsdl/ontario/ontology/'
+"""str: Prefix for the FedSDM ontology, i.e., federations, datasources, RDF Molecule Templates, etc."""
 MT_RESOURCE = 'http://tib.eu/dsdl/ontario/resource/'
+"""str: Prefix for resources of FedSDM, i.e., the prefix for actual instances."""
