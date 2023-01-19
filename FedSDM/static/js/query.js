c0730ed5e145829d08c2b301b36e1b74b58abc04
Philipp Rohde <Philipp.Rohde@tib.eu>
2023-01-19 10:45:04 +0100

    improve code style

--- FedSDM/static/js/query.js
+++ FedSDM/static/js/query.js
@@ -986,24 +986,18 @@ $(function() {
 
         function vis_by_node_score(score) {
             if (isNumber(score)) {
-                if (score >= 0.666)
-                    return keyh;
-                else if (score >= 0.333)
-                    return keym;
-                else if (score >= 0)
-                    return keyl;
+                if (score >= 0.666) { return keyh; }
+                else if (score >= 0.333) { return keym; }
+                else if (score >= 0) { return keyl; }
             }
             return true;
         }
 
         function vis_by_link_score(score) {
             if (isNumber(score)) {
-                if (score >= 0.666)
-                    return key3;
-                else if (score >= 0.333)
-                    return key2;
-                else if (score >= 0)
-                    return key1;
+                if (score >= 0.666) { return key3; }
+                else if (score >= 0.333) { return key2; }
+                else if (score >= 0) { return key1; }
             }
             return true;
         }
