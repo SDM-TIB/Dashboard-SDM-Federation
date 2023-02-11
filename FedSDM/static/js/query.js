de799b9458692ac98937d352ca6d4682bb6d8afa
Philipp Rohde <Philipp.Rohde@tib.eu>
2023-02-11 10:45:26 +0100

    move variable declaration to the top

--- FedSDM/static/js/query.js
+++ FedSDM/static/js/query.js
@@ -8,7 +8,8 @@ $(function() {
         queryTriples = [],
         vizData = {nodes: {}, links: []},
         queryVars = [],
-        table = null, selectedRow = null, selectedRowData = [];
+        table = null, selectedRow = null, selectedRowData = [],
+        keyc = true, keys = true, keyt = true, keyr = true, keyx = true, keyd = true, keyl = true, keym = true, keyh = true, key1 = true, key2 = true, key3 = true, key0 = true;
 
     if (federation != null && federation !== '') {
         $('#query_row').show();
@@ -709,8 +710,6 @@ $(function() {
 
     var width, height,
         h = 960, w = 760;
-
-    var keyc = true, keys = true, keyt = true, keyr = true, keyx = true, keyd = true, keyl = true, keym = true, keyh = true, key1 = true, key2 = true, key3 = true, key0 = true
 
     var focus_node = null, highlight_node = null;
 
