ce8e65c648ebfbec6b853f07b9a4f98b2f4b9a2c
Philipp Rohde <Philipp.Rohde@tib.eu>
2023-05-17 08:19:14 +0200

    use less lines

--- FedSDM/static/js/rdfmtviz.js
+++ FedSDM/static/js/rdfmtviz.js
@@ -288,9 +288,7 @@ $(function() {
 
                 o.source = nodes[o.source];
                 o.target = nodes[o.target];
-                if (o.source == null || o.target == null) {
-                    console.log(o)
-                }
+                if (o.source == null || o.target == null) { console.log(o) }
                 if (o.source.datasource === o.target.datasource) {
                     if (o.source.datasource in sourceLinks) {
                         sourceLinks[o.source.datasource].push(o);
