5560c5fc2a92de702f052f6e10717f07bc2b44f7
Philipp Rohde <Philipp.Rohde@tib.eu>
2023-08-07 08:07:23 +0200

    fix whitespace

--- FedSDM/rdfmtmgt.py
+++ FedSDM/rdfmtmgt.py
@@ -828,7 +828,7 @@ def get_graph_stat(graph: str = None, source: str = None) -> list:
     else:
         source = '"' + source + '" '
     if graph is not None:
-        query = 'SELECT distinct ?subject  ?target WHERE { graph <' + graph + '> {\n' \
+        query = 'SELECT distinct ?subject ?target WHERE { graph <' + graph + '> {\n' \
                 '  ?subject a mt:RDFMT .\n' \
                 '  ?subject mt:source ?source .\n' \
                 '  ?source mt:datasource ?datasource .\n' \
@@ -842,7 +842,7 @@ def get_graph_stat(graph: str = None, source: str = None) -> list:
                 '  }\n' \
                 '}}'
     else:
-        query = 'SELECT distinct ?subject  ?target WHERE {\n' \
+        query = 'SELECT distinct ?subject ?target WHERE {\n' \
                 '  ?subject a mt:RDFMT .\n' \
                 '  ?subject mt:source ?source .\n' \
                 '  ?source mt:datasource ?datasource .\n' \
