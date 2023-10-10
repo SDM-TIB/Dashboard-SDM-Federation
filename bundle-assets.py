6ef55f6a7c30b31ecf48dc390a37a50d1c5764de
Philipp Rohde <Philipp.Rohde@tib.eu>
2023-10-10 13:46:22 +0200

    remove dependency on flask-assets
    seems it is no longer actively maintained and it breaks with Flask v3.0+

--- bundle-assets.py
+++ bundle-assets.py
@@ -1,12 +1,72 @@
+import os.path
 import shutil
 
-from flask import Flask
-from flask_assets import Environment, Bundle
+from cssmin import cssmin
+from rjsmin import jsmin
 
-app = Flask(__name__, root_path='FedSDM/')
-assets = Environment(app)
 
-NPM_PATH = '../../node_modules/'
+class Bundle(object):
+    """A bundle is a unit to organize groups of media files, which filters to apply and where to store them.
+
+    This class is inspired by the class ``Bundle`` from the ``webassets`` library.
+    However, this class is only for the use within this project, and, hence, does not need to be general.
+    This leads to the advantage that an instance of this class does not need an ``Environment`` class for
+    translating relative paths to absolute ones. All paths are fixed for the use within this project.
+
+    We are not relying on ``flask_assets`` and ``webassets`` as it seems that they are not actively
+    maintained anymore. Additionally, those library do not work with Flask 3.0+.
+
+    """
+
+    STATIC_FOLDER = 'FedSDM/static/'
+
+    def __init__(self, *contents, **options):
+        self.contents = contents
+        self.output = options.pop('output', None)
+        self.filters = options.pop('filters', None)
+
+        if self.output is not None:
+            self.output = self.STATIC_FOLDER + self.output
+
+    def build(self):
+        """Builds the bundle.
+
+        All files are combined and stored at the specified location.
+        If any filters where given, they are applied before storing the combined bundle.
+
+        Other than with the original implementation of this method, the output will always be generated;
+        simulating a ``Bundle.build()`` call with ``force=True``.
+
+        Step 1: Read all input files into one string
+        Step 2: Apply filters
+        Step 3: Write output file
+
+        """
+        if self.contents is None or self.output is None:
+            return
+
+        content = ''
+        for file in self.contents:
+            if not os.path.isfile(file):
+                continue
+
+            content += open(file, 'r').read()
+            if not content.endswith('\n'):
+                content += '\n'
+
+        if self.filters == 'cssmin':
+            content = cssmin(content)
+        elif self.filters == 'rjsmin':
+            content = jsmin(content, keep_bang_comments=False)
+        else:
+            raise NotImplementedError('Filter ' + self.filters + ' not implemented.')
+
+        out_dir = os.path.dirname(self.output)
+        os.makedirs(out_dir, exist_ok=True)
+        open(self.output, 'w').write(content)
+
+
+NPM_PATH = 'node_modules/'
 JS_PATH = 'js/libs/'
 CSS_PATH = 'css/'
 WEBFONTS_PATH = 'webfonts/'
@@ -80,12 +140,8 @@ bundles = {
 }
 
 for name, bundle in bundles.items():
-    assets.register(name, bundle)
-    bundle.build(force=True)
+    bundle.build()
 
 # copy webfonts from Fontawesome
 shutil.rmtree('FedSDM/static/webfonts')
 shutil.copytree('node_modules/@fortawesome/fontawesome-free/webfonts', 'FedSDM/static/webfonts', dirs_exist_ok=True)
-
-# remove webassets cache
-shutil.rmtree('FedSDM/static/.webassets-cache')
